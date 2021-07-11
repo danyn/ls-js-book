052c3d27601a4ba06f7c89c85fdcf87446cf3fd2
Dan Eisen <eisen.dan@gmail.com>
2021-07-11 01:16:34 -0700

    progress

---
+++ preperations/test.js
@@ -0,0 +1,12 @@
+console.log("hello world!")
+console.log("Hi")
+
+function stylePage() {
+  document.body.style.backgroundColor = 'blue'
+  document.body.style.height = '100vh'
+}
+
+
+window.addEventListener('load', (event) => {
+  stylePage()
+});
